91b5b30ac2503063fa4d5e65c9f4fd9b395cfc88
John Anthony Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2024-10-02 01:15:15 -0700

    Day_1.6

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -49,4 +49,28 @@ print(movie.hasPrefix("A day"))
 print(filename.hasSuffix(".jpg"))
 
 
+let score = 10
+
+let reallyBig = 100_000_000
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubleScore = score * 2
+let squareScore = score * score
+let havledScore  = score/2
+
+var counter = 10
+counter = counter + 5
+counter += 5
+print(counter)
+
+counter *= 2
+counter -= 10
+counter /= 2
+
+let number = 120
+print(number.isMultiple(of: 3))
+print(120.isMultiple(of: 3))
+
+
 
