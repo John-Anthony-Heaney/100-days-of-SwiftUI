546ced8d9f081087f48f3e828b10f04f5056477d
John Anthony Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2024-10-02 01:42:16 -0700

    Day_2.1

---
+++ MyPlayground_2.playground/Contents.swift
@@ -0,0 +1,22 @@
+import Cocoa
+
+let filename = "paris.jpg"
+print(filename.hasSuffix(".jpg"))
+
+let number = 120
+print(number.isMultiple(of: 3))
+
+let goodDogs = true
+var gameOver = false
+print(gameOver)
+gameOver.toggle()
+print(gameOver)
+let isMultiple = 120.isMultiple(of: 3)
+
+var isAuthenticatd = false
+isAuthenticatd = !isAuthenticatd
+print(isAuthenticatd)
+isAuthenticatd = !isAuthenticatd
+print(isAuthenticatd)
+
+
