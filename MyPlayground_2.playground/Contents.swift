9a34ff5fc0c15c987097516eb0f5e29cad11cc1a
John Anthony Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2024-10-02 01:56:08 -0700

    Day_2.2

--- MyPlayground_2.playground/Contents.swift
+++ MyPlayground_2.playground/Contents.swift
@@ -3,7 +3,7 @@ import Cocoa
 let filename = "paris.jpg"
 print(filename.hasSuffix(".jpg"))
 
-let number = 120
+let num = 120
 print(number.isMultiple(of: 3))
 
 let goodDogs = true
@@ -20,3 +20,26 @@ isAuthenticatd = !isAuthenticatd
 print(isAuthenticatd)
 
 
+
+let firstPart = "Hello, "
+let secondPart = "world!"
+let greeting = firstPart + secondPart
+
+let people = "Haters"
+let action = "hate"
+let lyric = people + " gonna " + action
+print(lyric)
+
+let luggageCode = "1" + "2" + "3" + "4" + "5"
+
+let quote = "Then he tapped a sing saying \"believe\" and walked away."
+
+let name = "Taylor"
+let age = 26
+let message = "Hello, my name is \(name) and I'm \(age) years old."
+print(message)
+
+let number = 11
+let missionMessage = "Apollo \(number) landed on the moon."
+
+print("5 x 5 is \(5*5)")
