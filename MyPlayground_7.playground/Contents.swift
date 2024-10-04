6e6649a73dea91b07d0f9bf1295b6c4e61b9c44f
John Anthony Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2024-10-04 08:11:33 -0700

    Day_7.4

--- MyPlayground_7.playground/Contents.swift
+++ MyPlayground_7.playground/Contents.swift
@@ -75,3 +75,25 @@ let (firstName, lastName) = getUser()
 print("Name: \(firstName) \(lastName)")
 
 
+func hireEmployee(name: String) {}
+func hireEmployee(title: String) {}
+func hireEmployee(location: String) {}
+
+let lyric = "I see a red door and I want it painted black."
+print(lyric.hasPrefix("I see"))
+
+func isUppercase(_ string: String) -> Bool {
+    string == string.uppercased()
+}
+
+let string = "HELLO, WORLD"
+let result1 = isUppercase(string)
+
+func printTimesTable(for number: Int) {
+    for i in 1...12 {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTable(for: 5)
+
