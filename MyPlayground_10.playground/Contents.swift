623fc467e21197dc5f66acf81da292e7f135341a
John Anthony Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2024-10-09 11:21:03 -0700

    Day_10.2

--- MyPlayground_10.playground/Contents.swift
+++ MyPlayground_10.playground/Contents.swift
@@ -22,29 +22,25 @@ wings.printSummary()
 
 struct Employee {
     let name: String
-    var vacationRemaining = 14
+    var vacationAllocated = 14
+    var vacationTaken = 0
     
-    mutating func takeVacation(days: Int) {
-        if vacationRemaining > days {
-            vacationRemaining -= days
-            print("I'm going on vacation!")
-            print("Days remaining: \(vacationRemaining)")
-        } else{
-            print("Oops! There aren't enough days remaining.")
+    var vacationRemaining: Int {
+        get {
+            vacationAllocated - vacationTaken
+        }
+        
+        set {
+            vacationAllocated = vacationTaken + newValue
         }
     }
 }
 
-var archer = Employee.init(name: "Sterling Archer",  vacationRemaining: 14)
-archer.takeVacation(days: 5)
-print(archer.vacationRemaining)
+var archer = Employee(name: "Sterling Archer", vacationAllocated: 14)
+archer.vacationTaken += 4
+archer.vacationRemaining = 5
+print(archer.vacationAllocated)
 
-let a = 1
-let b = 2.0
-let c = Double(a) + b
-
-let kane = Employee(name: "Lana Kane")
-let poovey = Employee(name: "Pam Poovey", vacationRemaining: 35)
 
 
 
