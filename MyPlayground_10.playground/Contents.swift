956018016dd12dabebc66b4723f8ba4f7a39e6c5
John Anthony Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2024-10-09 11:40:34 -0700

    Day_10.4

--- MyPlayground_10.playground/Contents.swift
+++ MyPlayground_10.playground/Contents.swift
@@ -77,4 +77,25 @@ app.contacts.append("Allen W")
 app.contacts.append("Ish S")
 
 
+struct Player {
+    let name: String
+    let number: Int
+    
+    init(name: String) {
+        self.name = name
+        self.number = Int.random(in: 1...99)
+    }
+}
+
+let player = Player(name: "Megan R")
+print(player.number)
+
+
+
+
+
+
+
+
+
 
