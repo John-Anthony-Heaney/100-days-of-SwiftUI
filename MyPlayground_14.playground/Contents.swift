64a486242a1cf135884268b793acb69e4423cfa4
John Anthony Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2024-10-14 15:10:08 -0700

    Day_14.3

--- MyPlayground_14.playground/Contents.swift
+++ MyPlayground_14.playground/Contents.swift
@@ -60,5 +60,31 @@ if let unwrapped = myVar {
 //}
 
 
+let captains = [
+    "Enterprise": "Picard",
+    "Voyager": "Janeway",
+    "Defiant": "Sisko"
+]
+
+
+let new = captains["Serenity", default: "N/A"]
+
+let tvShows = ["Archer", "Babylon 5", "Ted Lasso"]
+
+let favorite = tvShows.randomElement() ?? "None"
+
+struct Book {
+    let title: String
+    let author: String?
+}
+
+let book = Book(title: "Beowolf", author: nil)
+let author = book.author ?? "Anonymous"
+print(author)
+
+let input = ""
+let number1 = Int(input) ?? 0
+print(number1)
+
 
 
