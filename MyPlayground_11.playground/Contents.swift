4ffe65f196ed91b172204df96043c25198ee506f
John Anthony Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2024-10-14 09:33:36 -0700

    Day_11.2

--- MyPlayground_11.playground/Contents.swift
+++ MyPlayground_11.playground/Contents.swift
@@ -28,4 +28,30 @@ if success {
     print("Failed to get the money")
 }
 
+struct School {
+    static var studentCount = 0
+    
+    static func add(student: String) {
+        print("\(student) joined the school.")
+        studentCount += 1
+    }
+}
+
+School.add(student: "Taylor Swift")
+print(School.studentCount)
+
+struct AppData {
+    static let version = "1.3 beta 2"
+    static let saveFilename = "setting.json"
+    static let homeURL = "https://www.hackingwithswift.com"
+}
+
+struct Employee {
+    let username: String
+    let password: String
+    
+    static let example = Employee(username: "cfederighi", password: "hgurowephr")
+}
+
+
 
