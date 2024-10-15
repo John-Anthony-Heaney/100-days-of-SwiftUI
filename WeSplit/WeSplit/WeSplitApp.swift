89a8a9a5af1b449341933d25b0779bf10bbaf42c
John Anthony Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2024-10-15 11:43:34 -0700

    Day_16.1

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by John Anthony Heaney on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
