ccbb236f593c088de8afb9073161d24fe36c11fb
John Anthony Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2024-10-15 12:18:25 -0700

    Day_16.4

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,24 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+    
     var body: some View {
-        NavigationStack {
-            Form {
-                Section{
-                    Text("Hello, world!")
-                }
-                Section {
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                }
-                Section {
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                }
-            }
+        Button("Tap Count: \(tapCount)") {
+            tapCount += 1
         }
-        .navigationTitle("SwiftUI")
     }
 }
 
