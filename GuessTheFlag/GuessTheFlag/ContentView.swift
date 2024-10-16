90b5fd6eec16394889e46926e28da5d19d83ccf1
John Anthony Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2024-10-16 09:29:05 -0700

    Day_20.1

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by John Anthony Heaney on 16/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack(alignment: .leading) {
+            Text("First")
+            Text("Second")
+            Text("Third")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
