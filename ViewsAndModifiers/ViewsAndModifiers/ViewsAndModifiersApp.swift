9a0891e0cacb00661869a05a32a75d8c26c0a22e
John Anthony Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2024-10-17 10:16:01 -0700

    Day_23.1

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by John Anthony Heaney on 17/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
