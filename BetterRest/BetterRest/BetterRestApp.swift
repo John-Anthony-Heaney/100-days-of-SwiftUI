e261e884c92efe42632fa7c73438d022733f633e
John Anthony Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2024-10-18 02:15:42 -0700

    Day_26.1

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by John Anthony Heaney on 18/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRest/BetterRestTests/BetterRestTests.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestTests.swift
+//  BetterRestTests
+//
+//  Created by John Anthony Heaney on 18/10/2024.
+//
+
+import Testing
+@testable import BetterRest
+
+struct BetterRestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
