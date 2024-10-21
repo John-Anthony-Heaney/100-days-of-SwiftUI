5a272aa2c06d288c0d712d50e52c0c220a6b251b
John Anthony Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2024-10-21 03:39:30 -0700

    Day_32.1

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by John Anthony Heaney on 21/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
