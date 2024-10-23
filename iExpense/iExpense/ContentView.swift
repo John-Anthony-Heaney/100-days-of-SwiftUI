3f7d4811454b0a18ca263328f08b945052e79c32
John Anthony Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2024-10-23 02:34:10 -0700

    Day_36.1

---
+++ iExpense/iExpense/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by John Anthony Heaney on 23/10/2024.
+//
+
+import SwiftUI
+
+@Observable
+class User {
+    var firstName = "Bilbo"
+    var lastName = "Baggins"
+}
+
+struct ContentView: View {
+    
+    @State private var user = User()
+    
+    var body: some View {
+        VStack {
+            Text("Your name is \(user.firstName) \(user.lastName)")
+            
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by John Anthony Heaney on 23/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
