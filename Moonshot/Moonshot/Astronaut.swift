9b3528811156fc35439795c17f4fcb2d451cfba3
John Anthony Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2024-11-06 08:31:50 -0800

    Day_40.1

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,16 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by John Anthony Heaney on 06/11/2024.
+//
+
+import Foundation
+
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let decription: String
+    
+}
