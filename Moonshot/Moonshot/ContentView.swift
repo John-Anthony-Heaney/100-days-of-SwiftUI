0298065ec4421c70433c16f525856c04a68b9f7d
John Anthony Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2024-11-08 10:29:36 -0800

    Day_42.4

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 
 
 struct ContentView: View {
+    
+    @State private var showingGrid = true
+    
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
@@ -19,11 +22,29 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack {
-            //GridLayout(astronauts: astronauts, missions: missions)
-            ListLayout(astronauts: astronauts, missions: missions)
-            .navigationTitle("Moonshot")
-            .background(.darkBackground)
-            .preferredColorScheme(.dark)
+            VStack {
+                
+                Button(action: {
+                    showingGrid.toggle()
+                }) {
+                    Text(showingGrid ? "Switch to List View" : "Switch to Grid View")
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+                
+                Group {
+                    if showingGrid {
+                        GridLayout(astronauts: astronauts, missions: missions)
+                    } else {
+                        ListLayout(astronauts: astronauts, missions: missions)
+                    }
+                }
+                .navigationTitle("Moonshot")
+                .background(.darkBackground)
+                .preferredColorScheme(.dark)
+            }
         }
     }
 }
--- Moonshot/Moonshot/ListLayout.swift
+++ Moonshot/Moonshot/ListLayout.swift
@@ -13,37 +13,35 @@ struct ListLayout: View {
     let missions: [Mission]
     
     var body: some View {
-        ScrollView {
-            List(missions) {mission in
-                NavigationLink {
-                    MissionView(mission:mission, astronauts: astronauts)
-                } label: {
-                    VStack(alignment: .leading){
-                        Image(mission.image)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 100, height: 100)
-                            .padding()
+        List(missions) { mission in
+            NavigationLink {
+                MissionView(mission: mission, astronauts: astronauts)
+            } label: {
+                VStack(alignment: .leading) {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                        .padding()
+                    
+                    VStack {
+                        Text(mission.displayName)
+                            .font(.headline)
+                            .foregroundStyle(.white)
                         
-                        VStack {
-                            Text(mission.displayName)
-                                .font(.headline)
-                                .foregroundStyle(.white)
-                            
-                            Text(mission.formattedLaunchDate)
-                                .font(.caption)
-                                .foregroundStyle(.white.opacity(0.5))
-                        }
-                        .padding(.vertical)
-                        .frame(maxWidth: .infinity)
-                        .background(.lightBackground)
+                        Text(mission.formattedLaunchDate)
+                            .font(.caption)
+                            .foregroundStyle(.white.opacity(0.5))
                     }
-                    .clipShape(.rect(cornerRadius: 10))
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(.lightBackground)
-                    )
+                    .padding(.vertical)
+                    .frame(maxWidth: .infinity)
+                    .background(.lightBackground)
                 }
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.lightBackground)
+                )
             }
         }
     }
