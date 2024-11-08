e5385117e01dd6f50523d33d7977e3fcea05174b
John Anthony Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2024-11-08 10:19:06 -0800

    Day_42.3

---
+++ Moonshot/Moonshot/ListLayout.swift
@@ -0,0 +1,58 @@
+//
+//  ListLayout.swift
+//  Moonshot
+//
+//  Created by John Anthony Heaney on 08/11/2024.
+//
+
+import SwiftUI
+
+struct ListLayout: View {
+    
+    let astronauts: [String:Astronaut]
+    let missions: [Mission]
+    
+    var body: some View {
+        ScrollView {
+            List(missions) {mission in
+                NavigationLink {
+                    MissionView(mission:mission, astronauts: astronauts)
+                } label: {
+                    VStack(alignment: .leading){
+                        Image(mission.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .padding()
+                        
+                        VStack {
+                            Text(mission.displayName)
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            
+                            Text(mission.formattedLaunchDate)
+                                .font(.caption)
+                                .foregroundStyle(.white.opacity(0.5))
+                        }
+                        .padding(.vertical)
+                        .frame(maxWidth: .infinity)
+                        .background(.lightBackground)
+                    }
+                    .clipShape(.rect(cornerRadius: 10))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(.lightBackground)
+                    )
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    let missions: [Mission] = Bundle.main.decode("Missions.json")
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    return ListLayout(astronauts: astronauts, missions: missions)
+        .preferredColorScheme(.dark)
+}
