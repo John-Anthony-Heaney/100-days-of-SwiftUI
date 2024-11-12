6ac4a03e78f1de1d65b7a1c12db1802a92e4229d
John Anthony Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2024-11-12 13:18:01 -0800

    Day_44.3

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -7,38 +7,31 @@
 
 import SwiftUI
 
-
-
-struct ContentView: View {
-   @State private var path = NavigationPath()
+struct DetailView: View {
+    var number: Int
+    
+    @Binding var path: NavigationPath
     
     var body: some View {
-        NavigationStack {
-            List {
-                ForEach(0..<5) { i in
-                    NavigationLink("Select Number: \(i)", value: i)
-                }
-                
-                ForEach(0..<5) { i in
-                    NavigationLink("Select String: \(i)", value: String(i))
-                }
-            }
+        NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
+            .navigationTitle("Number: \(number)")
             .toolbar {
-                Button("Push 556"){
-                    path.append(556)
-                }
-                
-                Button("Push Hello") {
-                    path.append("Hello")
+                Button("Home") {
+                    path = NavigationPath()
                 }
             }
-            
-            .navigationDestination(for: Int.self) { selection in
-                Text("You selected the number \(selection)")
-            }
-            .navigationDestination(for: String.self) { selection in
-                Text("You selected the string \(selection)")
-            }
+    }
+}
+
+struct ContentView: View {
+    @State private var path = NavigationPath()
+    
+    var body: some View {
+        NavigationStack(path: $path) {
+            DetailView(number: 0, path: $path)
+                .navigationDestination(for: Int.self) { i in
+                    DetailView(number: i, path: $path)
+                }
         }
     }
 }
