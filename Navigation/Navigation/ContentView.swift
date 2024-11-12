13cf2d535aa8f96f93e7ab559f03c3c43448e4ca
John Anthony Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2024-11-12 13:42:20 -0800

    Day_45.2

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -13,14 +13,19 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack {
-            List(0..<100) { i in
-                Text("Row \(i)")
-            }
-            .navigationTitle("Title goes here")
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbarBackground(.blue)
-            .toolbarColorScheme(.dark)
-            .toolbar(.hidden, for: .navigationBar)
+            Text("Hello, world!")
+                .toolbar {
+                    ToolbarItemGroup(placement: .topBarLeading) {
+                        Button("Tap me") {
+                        //Button action
+                        }
+                    
+                        Button("Tap me") {
+                        //Button action
+                        }
+                    }
+                }
+                .navigationBarBackButtonHidden()
         }
     }
 }
