837a95097b1e0512b2fcbb1e06c825077cf8033d
John Anthony Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2024-11-14 13:39:05 -0800

    Day_49.1

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by John Anthony Heaney on 14/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
