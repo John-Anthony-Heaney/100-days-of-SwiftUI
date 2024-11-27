c20c74ed16e33f4add8ac3daadf094d5f22e998f
John Anthony Heaney <99290690+John-Anthony-Heaney@users.noreply.github.com>
2024-11-27 05:23:16 -0800

    Day_53.1

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by John Anthony Heaney on 27/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
